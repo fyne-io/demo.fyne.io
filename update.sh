240757501f7de763dc83fbf07c0a8252022df1c1
Jacalz <jacalz@tutanota.com>
2025-06-09 10:48:23 +0200

    Update script to use new demo and regenerate

--- update.sh
+++ update.sh
@@ -1,22 +1,21 @@
 #!/bin/sh
 
 # Usage:
-# Pass the Fyne version in as argument to regenerate with that version.
-# Example: ./update.sh v2.5.3
+# Pass the demo version in as argument to regenerate with that version.
+# Example: ./update.sh v1.6.0
 
 # Exit on error:
 set -e
 
 # File path variables:
-URL="https://github.com/fyne-io/fyne/archive/refs/tags/$1.tar.gz"
+URL="https://github.com/fyne-io/demo/archive/refs/tags/$1.tar.gz"
 TEMP_DIR=$(mktemp -d)
 OUT_FILE="$TEMP_DIR/$1.tar.gz"
-DEMO_PATH="$TEMP_DIR/cmd/fyne_demo"
 
 # Download, extract, package and copy here before cleanup:
 wget -O $OUT_FILE $URL
 echo $OUT_FILE
 tar -xzf $OUT_FILE -C $TEMP_DIR --strip-components=1
-(cd $DEMO_PATH && ~/go/bin/fyne package --release --os=wasm)
-cp -rvf $DEMO_PATH/wasm/* .
+(cd $TEMP_DIR && ~/go/bin/fyne package -release -os=wasm)
+cp -rvf $TEMP_DIR/wasm/* .
 rm -rf $TEMP_DIR
