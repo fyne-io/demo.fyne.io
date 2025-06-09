02b72c51694c2cc1a512f51f891d1e66f5cdf148
Jacalz <jacalz@tutanota.com>
2025-06-09 10:51:19 +0200

    Use wasm-opt with -O2 to shave 2MB off of binary

--- update.sh
+++ update.sh
@@ -19,3 +19,4 @@ tar -xzf $OUT_FILE -C $TEMP_DIR --strip-components=1
 (cd $TEMP_DIR && ~/go/bin/fyne package -release -os=wasm)
 cp -rvf $TEMP_DIR/wasm/* .
 rm -rf $TEMP_DIR
+wasm-opt "Fyne Demo.wasm" --enable-bulk-memory-opt -O2 -o "Fyne Demo.wasm"
